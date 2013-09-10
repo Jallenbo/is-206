6e530252a2f7e8c838001a6c8006755c51bfb701
Jallenbo <jallis9025@gmail.com>
2013-09-11 01:39:46 +0200

    Oppgave 1 i Learn Python the hard way - ferdig

---
+++ ex1.py
@@ -0,0 +1,15 @@
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# DRILLS
+
+print "Another line."
+
+# 2. Comment out all lines but one
+
+# 3. # is for commenting
