3d82726031cea4473ac852f0f59d493cac37d71a
Jallenbo <jallis9025@gmail.com>
2013-09-11 13:56:54 +0200

    Oppgave 3 i Learn Python the hard way - ferdig

--- ex3.py
+++ ex3.py
@@ -1,14 +1,23 @@
 print "I will now count my chickens:"
 
+# 30 / 6 = 5
+# 5 + 25 = 30
 print "Hens", 25 + 30 / 6
+
+# 25 * 3 = 75
+# 75 % 4 = 3
+# -(25 * 3 % 4)= -3
+# 100 - 3 = 97
 print "Roosters", 100 -25 *3 % 4
 
 print "Now I will count the eggs:"
 
+# 1 + (4 % 2) - (1/4) + 6
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# Checks if 5 is smaller than -2, returns true or false
 print 3 + 2 < 5 - 7
 
 print "what is 3 + 2?", 3 + 2
@@ -18,6 +27,7 @@ print "Oh, that's why it's false."
 
 print "How about some more."
 
+# More comparing
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= -2
