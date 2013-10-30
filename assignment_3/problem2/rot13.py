8edcb55f2d8e9eefba0075406e871ca485ff5cd4
Jallenbo <jallis9025@gmail.com>
2013-10-31 00:51:08 +0100

    Changed from textbox to textarea

--- assignment_3/problem2/rot13.py
+++ assignment_3/problem2/rot13.py
@@ -3,24 +3,24 @@ import webapp2
 
 form = """
 <form method="post">
-<label>
-Do a rot13 on these words:
-<input type="text" name="rot13" value="%(rot13)s">
-</label>
+Do a rot13 on these words: <br />
+<textarea name="text" value="%(text)s">%(text)s</textarea> <br />
 <input type="submit" value="Submit">
+
 </form>
+
 """
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		self.write_form()
 
 	def post(self):
-		words = self.request.get("rot13")
+		words = self.request.get("text")
 		words = codecs.encode(words, 'rot_13')
 		self.write_form(words)
 	
-	def write_form(self, rot13=""):
-		self.response.out.write(form % {"rot13" : rot13})
+	def write_form(self, text=""):
+		self.response.out.write(form % {"text" : text})
 		
 application = webapp2.WSGIApplication([('/', MainPage)],
 									   debug=True)
